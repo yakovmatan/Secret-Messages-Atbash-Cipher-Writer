f975080be09cc56801792e5306e4621b9e24dc43
yakov matan <yakovmatan@gmail.com>
2025-05-26 10:30:08 +0300

    create save to file function

--- Program.cs
+++ Program.cs
@@ -8,8 +8,67 @@ namespace Secret_Messages___Atbash_Cipher_Writer
 {
     internal class Program
     {
+        static string Encrypt(string message)
+        {
+
+            string decrypted = "";
+            Dictionary<char, char> atbash = new Dictionary<char, char>()
+            {
+                {'a', 'z'},
+                {'b', 'y'},
+                {'c', 'x'},
+                {'d', 'w'},
+                {'e', 'v'},
+                {'f', 'u'},
+                {'g', 't'},
+                {'h', 's'},
+                {'i', 'r'},
+                {'j', 'q'},
+                {'k', 'p'},
+                {'l', 'o'},
+                {'m', 'n'},
+                {'n', 'm'},
+                {'o', 'l'},
+                {'p', 'k'},
+                {'q', 'j'},
+                {'r', 'i'},
+                {'s', 'h'},
+                {'t', 'g'},
+                {'u', 'f'},
+                {'v', 'e'},
+                {'w', 'd'},
+                {'x', 'c'},
+                {'y', 'b'},
+                {'z', 'a'}
+            };
+            foreach (char c in message)
+            {
+                if (atbash.ContainsKey(char.ToLower(c)))
+                {
+
+                    decrypted += atbash[char.ToLower(c)];
+
+
+                }
+                else
+                {
+                    decrypted += c;
+                }
+            }
+            return decrypted;
+
+        }
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a message to encrypt:");
+            string input = Console.ReadLine();
+
+            string encrypted = Encrypt(input);
+
+            Console.WriteLine("Encrypted message:");
+            Console.WriteLine(encrypted);
         }
     }
 }
